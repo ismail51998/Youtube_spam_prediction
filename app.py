e4a5e1cfeb812dc94e4791731f5a03b0193be248
ismail <ismail.moatadid@um6p.ma>
2021-05-22 00:32:42 +0100

    last

--- app.py
+++ app.py
@@ -2,6 +2,40 @@ from flask import Flask, render_template, url_for, request
 import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
+import string
+import csv
+import numpy as np
+from nltk.tokenize import word_tokenize
+from sklearn.metrics import classification_report
+
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+from nltk.tokenize import TweetTokenizer
+from string import punctuation
+
+
+def preprocess(corpus):
+    """Process tweet function.
+    Input:
+        tweet: a string containing a tweet
+    Output:
+        tweets_clean: a list of words containing the processed tweet
+
+    """
+    words=[]
+    stemmer = PorterStemmer()
+    for i in corpus:
+
+        word_tokens = [word.lower() for word in word_tokenize(i)]
+        t=""
+        for word in word_tokens:
+            if(not word in set(stopwords.words('english')) and  not word in string.punctuation and word.isalpha()):
+                t+=stemmer.stem(word.lower())+" "
+        words.append(t)
+    # tokenize tweets
+
+    return words
+
 
 # from sklearn.externals import joblib
 app = Flask(__name__)
@@ -21,7 +55,7 @@ def predict():
     df_x = df_data['CONTENT']
     df_y = df_data.CLASS
     # Extract the features with countVectorizer
-    corpus = df_x
+    corpus = preprocess(df_x)
     cv = CountVectorizer()
     X = cv.fit_transform(corpus)
     from sklearn.model_selection import train_test_split
@@ -30,6 +64,7 @@ def predict():
     clf = MultinomialNB()
     clf.fit(X_train, y_train)
     clf.score(X_test, y_test)
+    print(clf.score(X_test, y_test))
     if request.method == 'POST':
         comment = request.form['comment']
         data = [comment]
